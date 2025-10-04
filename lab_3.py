f77d229b08ffa7e6b496eed1028cc18e6fa1caa4
Ivan Voronko <ivan.voronko718@gmail.com>
2025-10-04 09:35:04 +0300

    Add task 7

--- lab_3.py
+++ lab_3.py
@@ -65,3 +65,22 @@ def financial_calculator():
     print(f"Общая прибыль: {profit.quantize(Decimal('0.01'))} руб.")
 
 financial_calculator()
+
+def fract():
+    from fractions import Fraction
+
+
+    frac1 = Fraction(3, 4)
+    frac2 = Fraction(5, 6)
+
+    addition = frac1 + frac2
+    subtraction = frac1 - frac2
+    multiplication = frac1 * frac2
+    division = frac1 / frac2
+
+    print(f"3/4 + 5/6 = {addition}")
+    print(f"3/4 - 5/6 = {subtraction}")
+    print(f"3/4 * 5/6 = {multiplication}")
+    print(f"3/4 / 5/6 = {division}")
+    pass
+fract()
