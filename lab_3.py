32ab02fccc079e242bdd7153d4566190f5c7d03f
Ivan Voronko <ivan.voronko718@gmail.com>
2025-10-04 08:42:22 +0300

    Add task 2

--- lab_3.py
+++ lab_3.py
@@ -2,3 +2,8 @@ def list_sqr():
     sqr = [x**2 for x in range(1, 10)]
     return sqr
 print('Список квадратов чисел от 1 до 10:',list_sqr())
+
+def list_chet():
+    chet = [x for x in range(1, 20) if x % 2 == 0]
+    return chet
+print('Список чётных чисел от 1 до 20:',list_chet())
