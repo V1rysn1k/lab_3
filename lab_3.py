5d1ecad5371c0ab38c73280c489dd0972689e051
Ivan Voronko <ivan.voronko718@gmail.com>
2025-10-04 08:51:03 +0300

    Add task 3

--- lab_3.py
+++ lab_3.py
@@ -7,3 +7,9 @@ def list_chet():
     chet = [x for x in range(1, 20) if x % 2 == 0]
     return chet
 print('Список чётных чисел от 1 до 20:',list_chet())
+
+def create_list_word():
+    words = ["python", "Java", "c++", "Rust", "go"]
+    up_words = [w.upper() for w in words if len(w) > 3]
+    return up_words
+print('Список слов в верхнем регистре и длиннее 3 символов:',create_list_word())
