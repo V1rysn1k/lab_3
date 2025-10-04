5468e1d5984eefd1424cf8a44c2682a107ec21ba
Ivan Voronko <ivan.voronko718@gmail.com>
2025-10-04 09:48:33 +0300

    Add task 10

--- lab_3.py
+++ lab_3.py
@@ -115,3 +115,21 @@ def birthday():
     print(f"Дней с момента рождения: {days_since_birthday}")
     print(f"Дней до следующего дня рождения: {days_until_next_birthday}")
 birthday()
+
+from datetime import datetime
+
+def format_datetime(dt: datetime) -> str:
+    day = dt.day
+    year = dt.year
+    months = [
+        "января", "февраля", "марта", "апреля", "мая", "июня",
+        "июля", "августа", "сентября", "октября", "ноября", "декабря"
+    ]
+    month_name = months[dt.month - 1]
+    time_str = f"{dt.hour:02d}:{dt.minute:02d}"
+
+    return f"Сегодня {day} {month_name} {year} года, время: {time_str}"
+
+
+now = datetime.now()
+print(format_datetime(now))
