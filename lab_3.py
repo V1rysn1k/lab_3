dd7f2162536c178b566d9c0cb78d84408fc627f7
Ivan Voronko <ivan.voronko718@gmail.com>
2025-10-04 09:42:45 +0300

    Add task 9

--- lab_3.py
+++ lab_3.py
@@ -95,3 +95,23 @@ def data_time():
     time = data_time.time()
     print("Текущее время:", time.strftime("%H:%M:%S"))
 data_time()
+
+def birthday():
+    from datetime import date, timedelta
+
+
+    birthday = date(2005, 9, 2)
+    today = date.today()
+    days_since_birthday = (today - birthday).days
+    this_year_birthday = birthday.replace(year=today.year)
+
+    if this_year_birthday < today:
+        next_birthday = this_year_birthday.replace(year=today.year + 1)
+    else:
+        next_birthday = this_year_birthday
+
+    days_until_next_birthday = (next_birthday - today).days
+
+    print(f"Дней с момента рождения: {days_since_birthday}")
+    print(f"Дней до следующего дня рождения: {days_until_next_birthday}")
+birthday()
