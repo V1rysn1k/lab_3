1ba4cfd96b3f34b1a2ce4b69d2748697b6fb672c
Ivan Voronko <ivan.voronko718@gmail.com>
2025-10-17 16:45:32 +0300

    Add task 1

---
+++ lab_4.py
@@ -0,0 +1,10 @@
+class Transport:
+    def __init__(self, brand, speed):
+        self.brand = brand
+        self.speed = speed
+
+    def move(self):
+        print(f"Transport is moving at {self.speed} km/h")
+
+    def __str__(self):
+        return f"Transport: {self.brand}, Speed: {self.speed}"
