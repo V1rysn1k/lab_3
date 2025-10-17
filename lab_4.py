413d3c34b8263f31ed019a3b2fe5158f6d78a6b8
Ivan Voronko <ivan.voronko718@gmail.com>
2025-10-17 16:49:16 +0300

    Add task 2

--- lab_4.py
+++ lab_4.py
@@ -8,3 +8,28 @@ class Transport:
 
     def __str__(self):
         return f"Transport: {self.brand}, Speed: {self.speed}"
+    
+class Car(Transport):
+    def __init__(self, brand, speed, seats):
+        super().__init__(brand, speed)
+        self.seats = seats
+
+    def honk(self):
+        print("Beep beep!")
+
+    def move(self):
+        print(f"Car {self.brand} is driving at {self.speed} km/h")
+
+    def __str__(self):
+        return f"Car: {self.brand}, Speed: {self.speed}, Seats: {self.seats}"
+    
+class Bike(Transport):
+    def __init__(self, brand, speed, type):
+        super().__init__(brand, speed)
+        self.type = type
+
+    def move(self):
+        print(f"Bike {self.brand} is cycling at {self.speed} km/h")
+
+    def __str__(self):
+        return f"Bike: {self.brand}, Speed: {self.speed}, Type: {self.type}"
